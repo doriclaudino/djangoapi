2639d263fe3afa728b02f6d79c72b1788eb8a370
dori claudino <dori.claudino@gmail.com>
2018-07-16 17:54:58 -0400

    using response

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,51 +1,49 @@
-from django.http import HttpResponse, JsonResponse, request
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from quickstart.models import Person
 from quickstart.serializers import PersonSerializer
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def person_list(request):
     """
-    List all code Persons, or create a new Person.
+    List all code person, or create a new person.
     """
     if request.method == 'GET':
         person = Person.objects.all()
         serializer = PersonSerializer(person, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        #data = JSONParser().parse(request)
-        serializer = PersonSerializer(data=request.POST)
+        serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def person_detail(request, pk):
     """
-    Retrieve, update or delete a code Person.
+    Retrieve, update or delete a code person.
     """
     try:
         person = Person.objects.get(pk=pk)
     except person.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PersonSerializer(person)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        #data = JSONParser().parse(request)
-        serializer = PersonSerializer(person, data=request.POST)
+        serializer = PersonSerializer(person, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         person.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
