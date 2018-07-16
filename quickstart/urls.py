10762274aa2b611d454e8c7bbebd6e8b512c110f
dori claudino <dori.claudino@gmail.com>
2018-07-16 18:51:50 -0400

    using class-based views

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -4,9 +4,9 @@ from quickstart import views
 
 
 urlpatterns = [
-    url(r'^person/$', views.person_list, name='person_list'),
+    url(r'^person/$', views.PersonList.as_view(), name='person_list'),
     url(r'^person/(?P<pk>[0-9]+)/$',
-        views.person_detail, name='person_detail'),
+        views.PersonDetail.as_view(), name='person_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
